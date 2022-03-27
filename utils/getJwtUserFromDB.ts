94c1c229f1ebe0da7cbad669eb90a60428c81d13
Buntu Cossie <buntucossie@gmail.com>
2022-03-27 22:43:11 +0200

    bug fix

--- utils/getJwtUserFromDB.ts
+++ utils/getJwtUserFromDB.ts
@@ -7,7 +7,7 @@ export async function getJwtUserFromDB(req: NextApiRequest) {
     let payload = authenticateUser(req)
     if (!payload) throw new ServerError("No token", 401)
     let username = payload.user
-    let user = await Users.findOne({username}) 
+    let user = await Users.findOne({username}).exec()
     if (!user) throw new ServerError("Unknown user", 401)
     return user
 }
