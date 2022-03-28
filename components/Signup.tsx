abcd4c1f660dd9b595feb0b15091a75e45713cf6
Buntu Cossie <buntucossie@gmail.com>
2022-03-28 15:41:30 +0200

    Added password strength meter

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { CSSProperties, useRef, useState } from "react"
 import {validateInput} from "../utils/validate"
 import styles from '../styles/Auth.module.css'
 import { useRouter } from "next/router"
@@ -12,6 +12,7 @@ export default function Signup() {
     const [password, setPassword] = useState("")
     const [password2, setPassword2] = useState("")
     const [errorMsg, setErrorMsg] = useState<string[]>([])
+    const ref = useRef<SVGSVGElement>(null)
     const router = useRouter()
     
     function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
@@ -41,7 +42,7 @@ export default function Signup() {
             console.log(e)
         }
     }
-    
+    let score = passwordStrength(password)
     return (
         <div className="flexColumn" style={{ width: "100%" }}>
             <form onSubmit={handleSubmit} className={styles.form}>
@@ -71,6 +72,10 @@ export default function Signup() {
                     placeholder="Password"
                     required
                 />
+            {password && fill(score)[0]} <br />
+            <svg ref={ref} width={'100%'} height={30}>
+                <rect style={{fill: fill(score)[1]}} height={10} width={Math.round(Math.max(0,Math.min(score, 100) / 100 * Number(ref.current?.width.baseVal.value)) || 0 )}  />
+            </svg>
                 <label htmlFor="signupPassword2">Confirm Password</label>
                 <input value={password2}
                     onChange={(e) => {
@@ -84,8 +89,9 @@ export default function Signup() {
                     placeholder="Confirm Password"
                     required
                 />
-                <button type="submit">Submit</button>
+                <button type="submit">Submit</button><br />
             </form>
+            
             {errorMsg.length > 0 &&
                 <ul className={styles.errorDiv}>
                     {errorMsg.map(error => <li key={error}>{error}</li>)}
@@ -93,4 +99,35 @@ export default function Signup() {
             }
         </div>
     )
+}
+
+function passwordStrength(pass: string) {
+    let score = 0;
+    let letters: {[key: string]: number} = {}
+
+    for (let letter of pass) {
+        letters[letter] = (letters[letter] || 0) + 1;
+        score += 5 / letters[letter]
+    }
+    
+    let regex = [/\d/, /[a-z]/, /[A-Z]/, /[^\da-zA-Z]/]
+
+    let variation = 0;
+    regex.forEach(reg => {
+        variation += (reg.test(pass) ? 1 : 0)
+    })
+
+    score += (variation - 1) * 10
+
+    return score
+}
+
+function fill(score: number) {
+    
+    if (score >= 100) return ["very strong", "darkgreen"]
+    if (score >= 80) return ["strong", "green"]
+    if (score >= 60) return ["good", "yellow"]
+    if (score >= 30) return ["weak", "orange"]
+    else return ["very weak", "red"]
+
 }
