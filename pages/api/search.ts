457ec30a4f71d35ccad24a98c4168dcabcd0a660
Buntu Cossie <buntucossie@gmail.com>
2022-04-05 16:32:43 +0200

    fixed search date

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method == 'POST') {
         const {searchTerm} = req.body
         const fromDate = req.body.fromDate ? new Date(req.body.fromDate) : new Date(2022,0,1)
-        const toDate = req.body.toDate ? new Date(req.body.toDate).setHours(23,59,59) : new Date().setHours(23,59,59)
+        const toDate = req.body.toDate ? new Date(req.body.toDate).setHours(23,59,59,999) : new Date().setHours(23,59,59,999)
         await mongoose.connect(process.env.MONGO_URI!)
         let posts = await Posts.find({title: {$regex: `${searchTerm}`, $options: 'i'}}).where('date').lte(toDate).gte(fromDate.getTime()).select('-user.password').exec()
         res.json({posts})
