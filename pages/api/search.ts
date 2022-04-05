44b66924ab1585f47d6c9a95a2f984dbfbcc5a17
Buntu Cossie <buntucossie@gmail.com>
2022-04-05 16:31:28 +0200

    fixed search date

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -5,11 +5,10 @@ import { Posts } from "../../utils/schema";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method == 'POST') {
         const {searchTerm} = req.body
-        const fromDate = req.body.fromDate || new Date(2022,0,1)
-        const toDate = req.body.toDate || new Date()
+        const fromDate = req.body.fromDate ? new Date(req.body.fromDate) : new Date(2022,0,1)
+        const toDate = req.body.toDate ? new Date(req.body.toDate).setHours(23,59,59) : new Date().setHours(23,59,59)
         await mongoose.connect(process.env.MONGO_URI!)
-        let posts = await Posts.find({title: {$regex: `${searchTerm}`, $options: 'i'}}).where('date').lte(toDate).gte(fromDate).select('-user.password').exec()
-        console.log(posts)
+        let posts = await Posts.find({title: {$regex: `${searchTerm}`, $options: 'i'}}).where('date').lte(toDate).gte(fromDate.getTime()).select('-user.password').exec()
         res.json({posts})
     }
 }
