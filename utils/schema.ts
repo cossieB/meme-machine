da25eef7ddd5aa4f53c655b4fdac4057951b9afc
Buntu Cossie <buntucossie@gmail.com>
2022-04-06 11:10:11 +0200

    Use MySQL
    Created a new branch to use MySQL instead of MongoDB

--- utils/schema.ts
+++ utils/schema.ts
@@ -1,32 +1,33 @@
 import mongoose from "mongoose";
-import type {IUser, IPost, IComment} from './interfaces'
+import type { IUser, IPost, IComment } from './interfaces'
 
-let {Schema} = mongoose
+let { Schema } = mongoose
 
 let UserSchema = new Schema<IUser>({
-    username: {type: String, required: true},
-    lowercase: {type: String, required: true},
-    password: {type: String, required: true},
-    joinDate: {type: Date, required: true},
-    avatar: {type: String, default: "/favicon.ico"},
-    status: {type: String, maxlength: [180, "Status has to be less than 180 characters"]}
+    username: { type: String, required: true },
+    lowercase: { type: String, required: true },
+    password: { type: String, required: true },
+    joinDate: { type: Date, required: true },
+    avatar: { type: String, default: "/favicon.ico" },
+    status: { type: String, maxlength: [180, "Status has to be less than 180 characters"] }
 })
 
 let PostSchema = new Schema<IPost>({
     title: {
-        type: String, 
-        require: true, 
-        maxlength: [30, "Title length has to be less than 30 characters"], minlength: [3, "Title length has to be at least 3 characters"]},
+        type: String,
+        require: true,
+        maxlength: [30, "Title length has to be less than 30 characters"], minlength: [3, "Title length has to be at least 3 characters"]
+    },
     image: {
-        type: String, 
+        type: String,
         required: true
     },
     description: {
-        type: String, 
+        type: String,
         maxlength: [200, "Description has to be less than 200 characters"]
-},
+    },
     user: {
-        type: UserSchema, 
+        type: UserSchema,
         required: true
     },
     date: Date,
@@ -34,8 +35,8 @@ let PostSchema = new Schema<IPost>({
 })
 
 let CommentSchema = new Schema<IComment>({
-    content: {type: String, required: true},
-    user: UserSchema, 
+    content: { type: String, required: true },
+    user: UserSchema,
     date: Date,
     post: String
 })
