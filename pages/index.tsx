d965141e1d239fb24771b8ebca9b40b3d7ce3494
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-05-30 12:30:15 +0200

    link fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,13 @@ export default function Home() {
                 </div> :
                 <div className={styles.links}>
                     <h2 style={{alignItems: 'center'}} className='label-header'>
-                        <Link href="/posts" >
+                        
                             <>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-columns-gap" viewBox="0 0 16 16">
                                     <path d="M6 1v3H1V1h5zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12v3h-5v-3h5zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8v7H1V8h5zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6v7h-5V1h5zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z" />
                                 </svg>
-                                Browse
+                            <Link href="/posts" > Browse </Link>
                             </>
-                        </Link>
                     </h2>
                     <h2 style={{alignItems: 'center'}} className='label-header'>
                         <>
