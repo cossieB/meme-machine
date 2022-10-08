1aa8e24fb6b75d2316cae1c1c4ce38bf31409453
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-10-08 15:19:37 +0200

    fix: fix broken style

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -3,7 +3,7 @@ import { IPost, IUser } from "../utils/interfaces";
 import styles from "../styles/Posts.module.scss"
 
 type PostExclUser = Omit<IPost, "user">
-type P = Partial<PostExclUser> & { user: Partial<IUser> } & { dateString: string } & { id: string } & {idx: number}
+type P = Partial<PostExclUser> & { user: Partial<IUser> } & { dateString: string } & { id: string } & { idx: number }
 
 interface Props {
     posts: P[],
@@ -15,13 +15,13 @@ const postsPerPage = 3;
 
 export default function Tile({ p }: { p: P }) {
     return (
-        <Link href={`/posts/${p.id}`}>
-            <a>
-                <div className={styles.tile}>
-                    <h3 style={{width: '100%'}}  className={`label-title-${p.idx}`} >{p.title}</h3>
+        <div className={styles.tile}>
+            <Link href={`/posts/${p.id}`}>
+                <a>
+                    <h3 style={{ width: '100%' }} className={`label-title-${p.idx}`} >{p.title}</h3>
                     <img src={p.image} alt={`${p.title} image`} />
-                </div>
-            </a>
-        </Link>
+                </a>
+            </Link>
+        </div>
     )
 }
