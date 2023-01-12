2f80033092f90961a83bb03ece0bc6c38bbf1e31
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-01-12 14:56:03 +0200

    refactor: starting from scratch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,38 +3,18 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import React, { createContext, useEffect, useState } from 'react'
 import cookie from 'cookie';
-import {  IUserContext, UserPick } from '../utils/interfaces';
+import { IUserContext, UserPick } from '../utils/interfaces';
+import { trpc } from '../utils/trpc';
 
 export const UserContext = createContext<IUserContext | null>(null)
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const [user, setUser] = useState<UserPick>()
-    useEffect(() => {
-        let cookies = document.cookie; 
 
-        if (cookie.parse(cookies).user) {
-            let storedUser: UserPick = JSON.parse(cookie.parse(cookies).user)
-            storedUser.joinDate = new Date(storedUser.joinDate)
-            setUser(storedUser)
-        }
-        else {
-            fetch('/api/login')
-                .then(res => res.json())
-                .then(data => {
-                    if (data.user) {
-                        setUser(data.user)
-                        document.cookie = `user=${JSON.stringify(data.user)}`
-                    }
-                })
-        }
-    }, [])
     return (
-        <UserContext.Provider value={{ user, setUser }}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </UserContext.Provider>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     )
 }
 
-export default MyApp
+export default trpc.withTRPC(MyApp)
